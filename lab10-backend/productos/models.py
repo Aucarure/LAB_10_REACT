111e5be37ffd941b2de30c7134c22f8c8b9699bb
Aucarure <victor.aucarure@tecsup.edu.pe>
2025-10-21 16:47:27 -0500

    first commit

---
+++ lab10-backend/productos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Producto(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    descripcion = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.descripcion
+    
+    class Meta:
+        db_table = 'productos'
---
+++ lab10-backend/productos/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Producto
+from .serializers import ProductoSerializer
+
+@api_view(['GET', 'POST'])
+def lista_productos(request):
+    if request.method == 'GET':
+        productos = Producto.objects.all()
+        serializer = ProductoSerializer(productos, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ProductoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
